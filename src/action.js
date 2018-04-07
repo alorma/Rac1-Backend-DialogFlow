3747b5401a5e718b55bd934ee5f2fd2638ce39f7
Bernat Borrás <bernatbor15@gmail.com>
2018-04-07 20:03:51 +0200

    Initial

--- src/action.js
+++ src/action.js
@@ -1,132 +1,78 @@
 var util = require('util');
-var swapi = require('./swapi');
-
+var rac1api = require('./rac1api');
 
 var process = async function(request) {
   const {originalRequest, result} = request;
-  const requestedCharacter = result.parameters.Subject;
-  const origin = processOriginalRequest(originalRequest);
   const action = result.action;
  
   let answer;
-  if (requestedCharacter == null) {
-      answer = "Tell me the name of a character";
-  } else {
-      let characters = await swapi.findPeople(requestedCharacter);
-      
-      if (!characters || characters.length == 0) {
-        answer  = 'I don\'t know who ' + requestedCharacter + ' is'
-      } else if  (characters.length > 1) {
-        let allButOne = characters.slice(0, characters.length - 2)
-        answer  = "Do you mean " + joinToPhrase(characters.map((character)=>character.name), "or")
-        context = "out"
-      } else {
-        let currentCharacter = characters[0]
-        answer = await getAnswerForAction(currentCharacter, action)
-        context = "out"
-      }
-  }
+  let contextOut
 
-  let contextOut = buildContextAnswer(requestedCharacter, answer, context);
+  console.info("Action: " + action)
+  
+  if (action == null) {
+    answer = "I not understand that"
+  } else {
+    answer = await getAnswerForAction(action)
+    contextOut = buildLiveContextAnswer(answer);
+  }
 
   return contextOut;
 }
 
-var buildContextAnswer = function(requestedCharacter, answer, context) {
+var buildLiveContextAnswer = function(answer) {
+  const program = answer.program
   return { 
-    speech: answer,
-    displayText: answer,
-    source: 'swBot',
-    contextOut: [
+    messages: [
       {
-        name: context,
-        Subject: requestedCharacter,
-      
+        platform: "google",
+        imageUrl: program.images.program,
+        description: program.description,
+        subtitle: program.subtitle,
+        title: program.title,
+        type: 1
+      },
+      {
+        type: 0,
+        speech: `Esta sonando ${program.title}`,
       }
     ]
   }
 }
 
-
-
-var getAnswerForAction = async function(character, action) {
+var getAnswerForAction = async function(action) {
   let answers 
+  let program
   switch(action) {
-    case 'subject.set' : 
+    case 'question.live' : 
+      program = await rac1api.now();
       answers = [
-          `What do you want to know about ${character.name}?`,
-          `Ok, what information do you want about ${character.name}?`,
-          `Cool one, I know a lot of things about ${character.name}, what do you want to know?`
+        `Esta sonando ${program.title}`,
+        `Ahora mismo suena ${program.title}`,
       ]
       break
-    case 'subject.get.height': 
-      answers = [
-        `${character.name} is ${character.height} cm tall`,
-        `${character.name} measures ${character.height} cm`
-      ]
-      break
-    case 'subject.get.hair': 
-      answers = [
-        `${character.name}'s hair color is ${character.hair_color}`,
-        `${character.name}'s has a ${character.hair_color} hair`,
-        `${character.name}'s hair is ${character.hair_color}`
-      ]
-      break
-    case 'subject.get.planet':
-      let homeworld = await swapi.findHomeWorld(character)
-      answers = [
-        `${character.name} was born in ${homeworld.name}`,
-        `${character.name} is from ${homeworld.name}`,
-        `${character.name} comes from ${homeworld.name}`
-      ]
-      break
-    case 'subject.get.movies':
-      let movies = await swapi.getMovies(character);
-      let moviesPhrase = joinToPhrase(movies, "and")
-      answers = [
-        `${character.name} appears in ${moviesPhrase}`,
-        `You can see ${character.name} on ${moviesPhrase}`
-      ]
-      break
+      case 'play.live' : 
+        program = await rac1api.now();
+        answers = [
+          `De acuerdo, reproduciendo, ${program.title}`
+        ]
+        break
     default:
       answers = [
-        `I don't know this information about ${character.name}`
+        `I don't know this`
       ]
   }
-  return pickupRandomPhrase(answers);
+  return {
+    program: program,
+    text: pickupRandomPhrase(answers)
+  }
 }
+
 
 var pickupRandomPhrase = function (array) {
   return array[Math.floor(Math.random() * Array.length)]
 }
 
-var processOriginalRequest = function(rawOriginalRequest) {
-  if (!rawOriginalRequest) {
-    return { 
-      type : 'none',
-      conversationId: null
-    }
-  } else if (rawOriginalRequest.source === "google") {
-    return {
-      type : 'google',
-      conversationId: rawOriginalRequest.data.conversation.conversationId
-    }
-  } else if (rawOriginalRequest.source === "telegram") {
-    return {
-      type: 'telegram',
-      conversationId: rawOriginalRequest.originalRequest.data.message.chat.id
-    }
-  }
-}
-
-var joinToPhrase = function(items, lastSeparator) {
-  console.log(items)
-  let allButOne = items.slice(0, items.length - 1)
-  console.log(allButOne)
-  return allButOne.join(", ") + " " + lastSeparator + " " + items[items.length - 1];
-}
-
 module.exports = {
   'process' : process
 };
-
--- src/index.js
+++ src/index.js
@@ -5,19 +5,18 @@ var bodyParser = require('body-parser');
 const app = express()
 app.use(bodyParser.json());
 
-app.post("/",   function(req, res)  {
-
-    body = req.body
-
-    action.process(body)
-    .then(function(response) { 
+app.post("/", function(req, res)  {
+  if (!action) {
+    console.error("Action is not defined")
+  } else {
+    action.process(req.body).then(function(response) { 
       res.statusCode = 200
       res.setHeader('Content-Type', 'application/json')
       res.send(response)
-    },
-    function(error) {
+    }, function(error) {
       console.log(error)
     })
+  }
 })
 
 app.listen(process.env.PORT || 8080);;
